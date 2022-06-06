6d8bdf874bb591349c19228b33117bd5949de666
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-06 06:30:11 -0700

    C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,6 @@
 #!/bin/bash
-int main(int argc, char *argv[]) {
-puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+			return (0);
 }
