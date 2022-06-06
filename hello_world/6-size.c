5d9f237caf033bd1f2457bb7c20f8f7591b23f2a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-06 08:27:02 -0700

    C

---
+++ hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+	char b;
+	float c;
+	double d;
+
+	printf("Size of a int: %zu byte(s)\n", sizeof(a));
+
+	return (0);
+}
