a478d8614d56f59f72b5315e47e9d0cad47f7c30
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-06 09:49:36 -0700

    C

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * main - Entry point
  * Return: Always 1
  */
 int main(void)
 {
-	fwrite("and that piece of art is useful\"
-	- Dora Korpar, 2015-10-19");
+	write(1,"and that piece of art is useful\"- Dora Korpar, 2015-10-19\n"61);
 
 	return (1);
 }
