e14dc365b5a837cd06f09b08092942bf9d6a2c3a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-06 06:41:55 -0700

    C

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 int main()
 { 
-	//string initialisation
 char Mystr [] = "Programming is like building a multilingual puzzle";
 puts (Mystr); //writing the string to stdout
 return 0;
