ebcbdcee95982add0c593a72d3ce5f99c18053ab
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-06 09:10:35 -0700

    C

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -6,6 +6,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(STDOUT_FILENO, "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19");
 	return (1);
 }
