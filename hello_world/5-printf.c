5040638f23a5b6079e03683ca99bdc1caf03be41
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-06 07:49:39 -0700

    C

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Return: Always 0 
+ * Return: Always 0
  */
 int main(void)
 { printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+return (0);
 }
