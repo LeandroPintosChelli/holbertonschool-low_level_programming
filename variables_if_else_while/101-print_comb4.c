de6426a4a4bcfd6a166105057a03f42e56a04731
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-07 16:39:17 -0700

    Variables

--- variables_if_else_while/101-print_comb4.c
+++ variables_if_else_while/101-print_comb4.c
@@ -10,21 +10,22 @@ int main(void)
 {
 	int x, y, z;
 
-for (x = 0; x < 8; x++)
+for (x = '0'; x < '8'; x++)
 {
-	for (y = x + 1; y < 9; y++)
+	for (y = x + 1; y < '9'; y++)
 {
-	for (z = y + 1; z < 10; z++)
+	for (z = y + 1; z < '10'; z++)
 {
 
 putchar((x % 10) + '0');
 putchar((y % 10) + '0');
 putchar((z % 10) + '0');
-	if (x == 7 && y == 8 && z == 9)
-	{
+
+if (x != '7' || y != '8' || z != '9')
+
 putchar(',');
 putchar(' ');
-}
+
 }
 }
 }
