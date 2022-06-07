5b8cfcc0d97481d77a814b8e3ab6e6f282e8f5b5
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-07 06:25:07 -0700

    Variables

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -8,16 +8,16 @@
  */
 int main(void)
 {
-	int x;
+int x;
 
-	for (x = '0'; x <= '9'; x++)
-	putchar(x);
+for (x = '0'; x <= '9'; x++)
+putchar(x);
 
-	if (x != '9')
+if (x != '9')
 {
-	putchar(',');
-	putchar(' ');
+putchar(',');
+putchar(' ');
 }
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
