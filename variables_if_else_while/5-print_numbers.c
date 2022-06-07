d6b31e741b188838180a281415b66e016dd1763e
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-07 05:47:03 -0700

    Variables

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+i#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+
+	for (x = 0; x < 10; x++)
+	printf("%d", x);
+	printf("\n");
+
+	return (0);
+}
