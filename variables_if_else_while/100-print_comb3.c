5a9a28c8495b8ce61ace8b5350c001f87cb2632b
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-07 09:21:10 -0700

    Variables

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -9,22 +9,21 @@
 int main(void)
 {
 int x, z;
-{
+
 for (x = '0'; x <= '9'; x++)
 {
 for (z = x + 1; z <= '9'; z++)
 {
 if (z != x)
-
+{
 putchar(x);
 putchar(z);
-
 if
 (x == '8' && z == '9')
-
+{
 putchar(' ');
 putchar(',');
-
+}
 }
 }
 }
