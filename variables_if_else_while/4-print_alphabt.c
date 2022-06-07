43a7a554c7544c78314616f6e16543b1b34f8a29
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-07 05:26:24 -0700

    Variables

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char az;
+
+	for (az = a..d, f..p, r..z az <= 'z'; az++)
+	putchar(az);
+	putchar('\n');
+	return (0);
+}
