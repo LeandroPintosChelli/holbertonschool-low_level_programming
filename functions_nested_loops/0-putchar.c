695ff98936b4e48e86a0979b2b11823e1ccf80ae
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-08 08:27:44 -0700

    functions

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * It prints the word Holberton, followed by a new line.
+ * Return:  Always 0
+ */
+int main(void)
+{
+char word[8] = "_putchar";
+int x;
+
+for (x = 0; x < 8; x++)
+	_putchar(word[x]);
+	_putchar('\n');
+	return (0);
+}
