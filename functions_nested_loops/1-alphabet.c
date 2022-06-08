179ef50f3cad470653b4ccedb423e144e842382e
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-08 09:04:08 -0700

    functions

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,12 +1,10 @@
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void);
 {
 char x;
 
