2c8cf331b2051168333e736e6c549d008350131f
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-09 07:31:24 -0700

    functions

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * int _abs - check the code
+ * @int: absolute value of an integer
+ * Return: Always 0.
+ */
+int _abs(int);
+{
+	int abs;
+	{
+	if (abs >= 0)
+	return ('abs');
+	else
+	return ('abs * -1');
+}
