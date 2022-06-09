8524076af2222e6649080cc8a4c018b214bd18cf
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-09 11:06:58 -0700

    functions

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,17 +10,27 @@ int j, i, n;
 
 	for (i = 0; i <= 9; i++)
 	{
+		_putchar(0 + '0');
 		_putchar(',');
 		_putchar(' ');
-		for (j = 0; j <= 9; j++)
+		for (j = 1; j <= 9; j++)
 		{
 			n = i * j;
-			if (j <= n - 1)
+			if (n < 10)
 			{
-			_putchar(i * j);
+				_putchar((n / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((n % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
-			_putchar(' ');
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
