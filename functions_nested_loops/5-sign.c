1d3e3b3d7f79311501aa629b0532425b5e4f5efa
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-09 07:16:34 -0700

    functions

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -7,7 +7,7 @@
  */
 int print_sign(int n)
 {
-	if (n < 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
@@ -17,7 +17,7 @@ int print_sign(int n)
 		_putchar(0);
 		return (0);
 	}
-	else if (n > 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 	}
