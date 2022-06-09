d3e69771c440b7bf8bddf45593bc3803247c8664
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-09 09:53:30 -0700

    functions

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,11 +12,11 @@ void jack_bauer(void)
 	{
 	for (m = 0; m < 60; m++)
 	{
-		_putchar((h % 10) + '0');
 		_putchar((h / 10) + '0');
+		_putchar((h % 10) + '0');
 		_putchar(':');
-		_putchar((m % 10) + '0');
 		_putchar((m / 10) + '0');
+		_putchar((m % 10) + '0');
 		_putchar('\n');
 }
 }
