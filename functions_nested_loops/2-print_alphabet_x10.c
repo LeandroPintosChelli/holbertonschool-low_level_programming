258a10359a0ed6466b77ed2836a2b014fe592ac9
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-09 06:10:59 -0700

    functions

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,12 +7,14 @@
  */
 void print_alphabet_x10(void)
 {
-int i;
+char az, i;
+int i = 0
 
-for (i = 0; i < 10; i++)
-
-_putchar(i);
+while (i < 10; i++)
+for (az = 'a'; az <= 'z'; az++)
+{
+_putchar(az);
+}
 _putchar('\n');
 return ();
-
 }
