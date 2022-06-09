dd24190d5dfa6908e97c879d28899a34a11b9a9a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-09 07:11:32 -0700

    functions

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -14,14 +14,14 @@ int n;
 	_putchar(+);
 	return (1);
 }
-	if (n == 0)
+else if (n == 0)
 {
 	_putchar(0);
 	return (0);
 }
-	if (n > 0)
+else if (n > 0)
 {
 	_putchar('-');
-	return ('-1');
+	return (-1);
 }
 }
