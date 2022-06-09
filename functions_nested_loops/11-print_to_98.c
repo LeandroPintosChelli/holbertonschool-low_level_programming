18f149e0d99ec6d435bf7c9267d17c2184c061d8
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-09 12:25:50 -0700

    functions

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -8,7 +8,7 @@ void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-		for (n <= 98; n++)
+		for (n = 0; n <= 98; n++)
 		{
 			_putchar(n);
 		}
@@ -20,7 +20,7 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (n >= 98; n--)
+		for (n = 0; n >= 98; n--)
 		{
 			_putchar(n);
 		}
