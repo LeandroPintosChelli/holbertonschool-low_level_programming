6dc61340157850b13f42452128457fb6116e2b89
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 06:15:00 -0700

    more_functions

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,10 @@
  */
 void print_numbers(void)
 {
-int n;
+	int n;
 
-	for (n = 0; n < 10; n++)
-	{
-		_putchar(n);
-		_putchar(10);
-	}
+	for (n = '0'; n <= '9'; n++)
+	_putchar(n);
+	_putchar('\n');
 	return (0);
 }
