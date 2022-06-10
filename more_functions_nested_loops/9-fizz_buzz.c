e38f0532740fee5c4a7d5eff9c3eef6a6ae528c6
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 09:02:42 -0700

    more_functions

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	if (n % 5 == 0 && n % 3 == 0)
+		printf("Fizzbuzz");
+}
+
+
+
