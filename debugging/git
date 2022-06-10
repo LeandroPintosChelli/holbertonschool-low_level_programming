0fdd01ff0e8eaf4a2217b79b17d8d927fbc5586c
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 14:14:44 -0700

    debugging

---
+++ debugging/git
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m "debugging"
+git push
---
+++ debugging/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int main(void);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+#endif
