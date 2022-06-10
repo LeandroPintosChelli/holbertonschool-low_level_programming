4a6cd9e8591abd1c2b30b9711420e8535e40ecb9
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 09:44:25 -0700

    more_functions

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -15,10 +15,12 @@ if (n % 3 == 0 && n % 5 == 0)
 printf("FizzBuzz ");
 else
 if (n % 3 == 0)
-printf("Fizz ");
+printf("Fizz");
+if (n != 100)
+	printf(' ');
 else
 if (n % 5 == 0)
-printf("Buzz ");
+printf("Buzz");
 else
 printf("%d ", n);
 }
