4159d652d1da60ccf7dce33f19330ce64f32a030
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 10:13:37 -0700

    more_functions

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -7,21 +7,21 @@
  */
 int main(void)
 {
-int n;
+	int n;
 
-for (n = 1; n <= 100; n++)
-{
-if (n % 3 == 0 && n % 5 == 0)
-printf("FizzBuzz ");
-else
-if (n % 3 == 0)
-printf("Fizz ");
-else
-if (n % 5 == 0)
-printf("Buzz ");
-else
-printf("%d ", n);
-}
-printf("Buzz\n");
-return (0);
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+			printf("FizzBuzz ");
+		else
+		if (n % 3 == 0)
+			printf("Fizz ");
+		else
+		if (n % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", n);
+	}
+	printf("\n");
+	return (0);
 }
