8e8574138218a914af03f2379e1e8a5a5931b4e6
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 14:43:24 -0700

    debugging

--- debugging/1-main.c
+++ debugging/1-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - while (i < 10)
+ * main - causes an infinite loop
  * Return: 0
  */
 
@@ -13,10 +13,10 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10)
+	/*while (i < 10)
 	{
 		putchar(i);
-	}
+	*/}
 
 	printf("Infinite loop avoided! \\o/\n");
 
