2aa72379048defc48bf2d8793dfecbd5ee26222c
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 06:10:19 -0700

    more_functions

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+int n;
+
+	for (n = 0; n < 10; n++)
+		_putchar(n);
+		_putchar(10);
+	return (0);
+}
