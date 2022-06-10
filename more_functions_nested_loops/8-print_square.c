f22cffcdc0686f5447f4137f4d58b345f3952f5f
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 08:05:03 -0700

    more_functions

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -12,9 +12,9 @@ void print_square(int size)
 	{
 		for (a = 0; a < size; a++)
 		{
-			for (b = 0; b < a; b++)
-			_putchar('\n');
+			for (b = 0; b < size; b++)
 			_putchar(35);
+			_putchar('\n');
 		}
 	}
 	else
