8f57d9bb97d19a2dace38d074a99f9f0f42657d5
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-10 10:14:36 -0700

    more_functions

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,15 +13,14 @@ int main(void)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz ");
-		else
-		if (n % 3 == 0)
+		else if (n % 3 == 0)
 			printf("Fizz ");
-		else
-		if (n % 5 == 0)
+		else if (n % 5 == 0)
 			printf("Buzz ");
 		else
 			printf("%d ", n);
 	}
 	printf("\n");
+
 	return (0);
 }
