9f726c3a1adab4507bcc6d5a86c5498114317c06
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-14 10:38:58 -0700

    pointers

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include <string.h>
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strlen - check the code
+ * @s: Write a function that returns the length of a string.
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s != 0; i++);
+	_putchar(i);
+	_putchar("%d")
+		return(0);
+}
