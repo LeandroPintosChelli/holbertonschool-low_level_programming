2f0e5694280eb060e33f664c5d9cab2e06e15888
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-14 11:01:46 -0700

    pointers

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _puts - check the code
+ * @s: Write a function that returns the length of a string.
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	puts (str);
+}
