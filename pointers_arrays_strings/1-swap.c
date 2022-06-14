13faa91236f3c0ca68c5e686939a7b1c0c2f2d3c
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-14 10:12:25 -0700

    pointers

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
  * reset_to_98 - check the code
- * @n: takes a pointer to an int as parameter and updates
+ * @a: swaps the values of two integers.
+ * @b: swaps the values of two integers.
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
