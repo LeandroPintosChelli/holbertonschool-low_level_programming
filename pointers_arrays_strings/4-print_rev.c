63e10ee671fe3ae87e9e3a9bdd2498a6ec6ae89c
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-14 11:56:51 -0700

    pointers

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * print_rev - check the code
+ * @s: Write a function that returns the length of a string in reverse.
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	char *i = s;
+
+	while (*(i + 1) != '\0')
+		i++;
+
+	while (i > s)
+	{
+		char j = *i;
+		*i-- = *s;
+		*s++ = j;
+	}
+}
