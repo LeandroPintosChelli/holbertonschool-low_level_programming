5d64e4a35f105bbb19e553f93e683465045dd189
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-14 13:40:38 -0700

    pointers

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,12 +10,12 @@ void rev_string(char *s)
 {
 	int i, len, temp;
 
-	len = strlen(*s);
+	len = strlen(s);
 
 	for (i = 0; i < len / 2; i++)
 	{
 		temp = *s
-			*s[i] = *s[len - i - 1];
+		*s[i] = *s[len - i - 1];
 		*s[len - i - 1] = temp;
 	}
 }
