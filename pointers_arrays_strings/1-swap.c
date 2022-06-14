4d282daf81e3a742340aeda6cf4b10c0a7a5402d
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-14 10:07:39 -0700

    pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reset_to_98 - check the code
+ * @n: takes a pointer to an int as parameter and updates
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 10;
+	*b = 20;
+}
