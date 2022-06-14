109f023ceb968f71923238192c4f8417c1fbc6ef
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-14 13:19:00 -0700

    pointers

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - check the code
+ * @s: Write a function that returns a string in reverse.
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		i++;
+	}
+	while (i >= 0)
+	{
+		s--;
+		i--;
+		if (*s != '\0')
+		_putchar(*s);
+	}
+	putchar('\n');
+}
