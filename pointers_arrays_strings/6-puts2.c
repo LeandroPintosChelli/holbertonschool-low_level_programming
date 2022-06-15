aea2b2af290337027894970970460951123e8fde
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-15 10:19:55 -0700

    pointers

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,12 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (*str != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		i += 2;
-		str++;
+	if (i % 2 == 0)
+		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
