5f103515fccdc0d3adc8e6769910fcda03346cba
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-15 11:41:54 -0700

    pointers

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -5,16 +5,17 @@
  * _strcpy - check the code
  * @dest: a
  * @src: copies the string pointed
+ * @n: sas
  * Return: Always 0.
  */
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, char *src, size_t n)
 {
 	size_t i;
-	size_t n;
-	for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[i] = src[i];
-	for ( ; i < n; i++)
-	dest[i] = '\0';
 
-	return dest;
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n; i++)
+		dest[i] = '\0';
+
+	return (dest);
 }
