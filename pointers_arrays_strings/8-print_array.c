3407e559b635e0c9282d101c54fc0d700cc7939c
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-15 10:57:31 -0700

    pointers

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * rev_string - check the code
+ * @s: Write a function that returns a string in reverse.
+ * Return: Always 0.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != (n - 1))
+		printf("%d, ", a[i]);
+	else
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
