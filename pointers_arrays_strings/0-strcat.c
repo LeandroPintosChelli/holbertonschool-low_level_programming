8195fdb956bb5c1100bc3aaeb3d6555725cad988
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-16 05:37:41 -0700

    pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,10 @@
  */
 char *_strcat(char *dest, char *src)
 {
-	size_t n = 0;
 	size_t dest_len = strlen(dest);
 	size_t i;
 
-	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	for (i = 0 ; src[i] != '\0'; i++)
 	dest[dest_len + i] = src[i];
 	dest[dest_len + i] = '\0';
 
