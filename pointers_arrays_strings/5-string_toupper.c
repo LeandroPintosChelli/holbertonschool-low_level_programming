1dfbfd84e2773b7fbc08deb1c0af6b912a9b9f3f
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-16 10:46:10 -0700

    pointers

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -3,17 +3,17 @@
 #include <string.h>
 #include <stddef.h>
 /**
- * reverse_array - check the code
- * @a: a
- * @n: Write a function that reverses the content of an array of integers.
+ * string_toupper - check the code
+ * @s: function that changes all lowercase letters of a string to uppercase.
  * Return: Always 0.
  */
 char *string_toupper(char *s)
 {
-int i = 0;
+	int i = 0;
+
 	if (*s >= 'a' && *s <= 'z')
 	{
-		s[i] = s[i] - 32;
+	s[i] = s[i] - 32;
 	}
 	s++;
 }
