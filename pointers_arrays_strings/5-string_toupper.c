4eb97dba4708fa3e180b85bef9c4e452691aa858
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-16 10:42:16 -0700

    pointers

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * reverse_array - check the code
+ * @a: a
+ * @n: Write a function that reverses the content of an array of integers.
+ * Return: Always 0.
+ */
+char *string_toupper(char *s)
+{
+int i = 0;
+	if (*s >= 'a' && *s <= 'z')
+	{
+		s[i] = s[i] - 32;
+	}
+	s++;
+}
