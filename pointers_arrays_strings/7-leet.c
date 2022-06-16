199e9a262f390ea4bc3250b1045178cca091589d
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-16 11:28:31 -0700

    pointers

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -3,15 +3,14 @@
 #include <string.h>
 #include <stddef.h>
 /**
- * _strcpy - check the code
- * @dest: a
- * @src: copies the string pointed
+ * leet - check the code
+ * @s: Write a function that encodes a string into 1337.
  * Return: Always 0.
  */
 char *leet(char *s)
 {
-	int a[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-	int b[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int i, j;
 
 	for (j = 0; s[j] != '\0'; j++)
