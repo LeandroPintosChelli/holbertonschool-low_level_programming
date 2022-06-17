34e0e889c1b49332a552557d6cd33c62fe6c3c77
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-17 12:48:31 -0700

    pointers

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * print_chessboard - check the code
+ * @a: print_diagsums
+ * @size: a
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size);
+{
+	int i;
+	int j = 0;
+	int k = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		j += a[i];
+		a += size;
+	}
+
+	a -= size;
+
+	for (i = 0; i < size; i++)
+	{
+		k += a[i];
+		a -= size;
+	}
+
+	printf("%d, %d\n", j, k);
+}
