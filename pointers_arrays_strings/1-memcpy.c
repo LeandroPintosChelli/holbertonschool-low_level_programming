31dd644a38c523e003ebe1ecb2f0df661f213b34
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-17 06:37:18 -0700

    pointers

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+/**
+ * _strcpy - check the code
+ * @dest: a
+ * @src: copies the string pointed
+ * Return: Always 0.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int bytes = n;
+	int i;
+
+	if (bytes > 0)
+	{
+		for (i = 0; i < bytes; i++)
+		s[i] = b;
+	}
+	return (s);
+}
