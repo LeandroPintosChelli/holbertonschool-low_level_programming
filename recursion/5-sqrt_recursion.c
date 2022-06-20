c3b5894e618e9faaa2aa962ab31639e9b2f26445
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-20 11:26:29 -0700

    Recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -10,9 +10,9 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	else if (n != 0 || n > 1)
+	else if (n > 0 || n > 1)
 	{
-		return (1);
+		return (_sqrt_recursion(n + 1));
 	}
-	return (_sqrt_recursion(n + 1));
+	return (1);
 }
