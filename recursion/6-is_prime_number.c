f5436002938d459467fbc13c3265df9718169f1c
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-20 12:45:41 -0700

    Recursion

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,8 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * factorial - check the code
- * @n: prints a string in reverse.
+ * prime - check the code
+ * @a: a
+ * @b: returns 1 if the input integer is a prime number
  * Return: Always 0.
  */
 int prime(int a, int b)
@@ -11,12 +12,12 @@ int prime(int a, int b)
 		return (1);
 	if (a % b == 0)
 		return (0);
-	return (prime (a, b + 1));
+	return (prime(a, b + 1));
 }
 /**
-*
-*
-*
+* is_prime_number - check the code
+* @n: returns 1 if the input integer is a prime numbe
+* Return: Always
 */
 int is_prime_number(int n)
 {
