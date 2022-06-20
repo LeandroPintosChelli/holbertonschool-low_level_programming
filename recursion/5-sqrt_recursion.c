f9f541a6136b47681f606b28d899e1abd5cb5428
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-20 11:19:05 -0700

    Recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -2,16 +2,17 @@
 #include <stdio.h>
 #include <math.h>
 /**
- * main - check the code
- *
+ * _sqrt_recursion - check the code
+ * @n: square root
  * Return: Always 0.
  */
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 1)
-		return (0);
- 
-	return (_sqrt_recursion(n + 1));
+	if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	return(_sqrt_recursion(n + 0));
 }
