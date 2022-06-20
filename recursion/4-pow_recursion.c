9d528a0bb50d4c8208b463905b2deae9cc7eb132
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-20 09:31:26 -0700

    Recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,14 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _strlen_recursion- check the code
- * @s: function that returns the length of a string
+ * _pow_recursion - check the code
+ * @x: returns the value of x
+ * @y: raised the power of y
  * Return: Always 0.
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else
-		return (x * _pow_recursion(x, y -1));
+
+	if (x == 0)
+		return (0);
+
+	if (x == 1 || y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y - 1));
 }
