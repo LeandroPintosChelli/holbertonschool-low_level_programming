07b1fd31830838595f368f085747270233db9feb
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-20 09:08:41 -0700

    Recursion

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,13 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _print_rev_recursion - check the code
- * @s: prints a string in reverse.
+ * factorial - check the code
+ * @n: prints a string in reverse.
  * Return: Always 0.
  */
 int factorial(int n)
 {
-	if (n > 0)
+	if (n < 0)
 		return (-1);
 
 	else if (n == 0 || n == 1)
