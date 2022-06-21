090d2dcfd13159fa25e725c343442d3c44f04c22
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-21 10:42:54 -0700

    argc-argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - multiplicate
+* @ac: int
+* @av: char
+* Return: Always 0
+*/
+int main(int ac, char *av[])
+{
+	if (ac != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", atoi(av[1]) * atoi(av[2]));
+	return (0);
+}
