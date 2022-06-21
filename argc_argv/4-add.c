5a755fead33aa5e894d9fcc9b4c83396d0901b34
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-21 11:45:29 -0700

    argc-argv

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -10,6 +10,7 @@
 int main(int ac, char *av[])
 {
 	int i, k = 0;
+	int j = 0;
 
 	if (ac < 2)
 	{
@@ -17,7 +18,13 @@ int main(int ac, char *av[])
 		return (0);
 	}
 	for (i = 1; i < ac; i++)
-
+		{
+			if (isalpha(av[i][j] > 0))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
 		k = k + atoi(av[i]);
 	printf("%d\n", k);
 	return (0);
