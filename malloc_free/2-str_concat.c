adce233507e98a7d963c816acbadc11099a3d633
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-23 15:27:35 -0700

    malloc_free

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -15,10 +15,13 @@ char *str_concat(char *s1, char *s2)
 	char b = strlen(s2);
 
 	if (s1 == NULL)
-	s1 = "";
+	{
+		return (NULL);
+	}
 	if (s2 == NULL)
-	s2 = "";
+		s2 = "";
 	c = (char *)malloc(a + b + 1);
+
 	for (i = 0; s1[i]; i++)
 	{
 		c[i] = s1[i] + s2[i];
