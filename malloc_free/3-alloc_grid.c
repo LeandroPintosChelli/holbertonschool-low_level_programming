40ead4e81f9bcfa9820f3e34dbe67949b619b298
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-23 16:39:51 -0700

    malloc_free

---
+++ malloc_free/3-alloc_grid.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* _strdup - check code
+* @str: duplicate string
+* Return: Always 0
+*/
+int **alloc_grid(int width, int height)
+{
+	int i = 0;
+	char *a = 0;
+	char *b = 0;
+	char *c = 0;
+
+	if (width <= 0)
+	{
+		return (NULL);
+	}
+	if (height <= 0)
+	{
+		return (NULL);
+	}
+	for (width = 0; width < i; width++)
+	{
+		a[width] = b[width];
+	}
+	for (height = 0; height < i; height++)
+	{
+		b[height] = c[height];
+	}
+	return (NULL);
+}
