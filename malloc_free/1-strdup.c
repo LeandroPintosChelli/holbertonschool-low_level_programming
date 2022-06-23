22801a7352b97823123bb1c73bf45c83ab24a1a9
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-23 09:49:44 -0700

    malloc_free

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*
+*
+*/
+char *_strdup(char *str)
+{
+	int s;
+	char *c;
+	char *x;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	s = strlen(str);
+	c = (char *)malloc(sizeof(char))
+
+	if (c == NULL)
+	{
+		for (s = 0; s < str; s++)
+		return (NULL);
+	}
+	return (s);
+} 
