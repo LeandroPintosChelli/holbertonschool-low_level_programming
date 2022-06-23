cb1afd852d9ce71d263ea02032e31a855339a1a6
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-23 06:28:07 -0700

    malloc_free

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* *create_array - check the code
+* @size: a
+* @c: b
+* Return: Always 0
+*/
+char *create_array(unsigned int size, char c)
+{
+	int size = i;
+	int j;
+	char *s;
+
+	if (size == 0)
+	{
+		for (j = 0; j < size; j++)
+		s[j] = c;
+		return (NULL);
+	}
+	else
+		if (size != 0)
+		{
+			return (NULL);
+		}
+}
