e115c55c0e47497f4edc175fcc0b44aa645bda55
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-24 09:52:35 -0700

    more_malloc_free

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -24,9 +24,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 	{
-		return (0);
+		a[i] = 0;
 	}
-	return (NULL);
+	return (a);
 }
