a71487b91524e2aa518e58b3c7f8398c4d97fd2a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-24 09:29:59 -0700

    more_malloc_free

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* _calloc - check code
+* @nmemb: int
+* @size: int
+* Return: Always 0
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i;
+	char *x;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+
+	x = malloc(sizeof(char) * ((nmemb + size) + 1));
+
+	if (x == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		x[nmemb] = x[size];
+		size++;
+	}
+	return (x);
+}
