5d319b4965674505f91c1f065bc2c5d3c4c57ae8
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-24 06:25:31 -0700

    more_malloc_free

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+*
+*
+*
+*/
+void *malloc_checked(unsigned int b)
+{
+	char *a = 0;
+
+	for (b = 0; a[b]; b++)
+	{
+		a =(char *)malloc(sizeof(char) * b);
+		exit(0);
+	}
+	return (a);
+}
