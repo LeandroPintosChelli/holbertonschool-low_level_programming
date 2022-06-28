53146f7336f16bf81082fb932c2add5f28235e6a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-28 06:54:33 -0700

    structures

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,32 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* _strlen - check code
+* @str: char
+* Return: Always 0
+*/
+int _strlen(char *str)
+{
+	int count = 0;
+
+	for (count = 0; count < str; count++)
+		return (count);
+}
+/**
+* new_dog - check code
+* @name: char
+* @age: float
+* @owner: char
+* Return: Always 0
+*/
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *firulais;
+
+	if (name == NULL || owner == NULL)
+		return (NULL);
+	firulais = malloc(size_t(dog_t);
+	if (firulais ==  NULL)
+		return (NULL)
+}
