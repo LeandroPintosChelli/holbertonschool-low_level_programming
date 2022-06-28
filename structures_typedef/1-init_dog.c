eab5d6814fe3914d9c4eed83e5ac07fbe34d6780
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-27 18:02:10 -0700

    structures

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,12 +8,12 @@
 * @age: age
 * @owner: owner
 */
-struct dog
+/**struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+};*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
