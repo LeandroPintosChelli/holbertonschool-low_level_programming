925d21830aadfa36df43dab9c166f06461fe7add
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-27 18:07:48 -0700

    structures

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - define new type
+* @name: name
+* @age: age
+* @owner: owner
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
