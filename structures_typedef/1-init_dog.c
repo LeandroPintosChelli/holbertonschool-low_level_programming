807461f50d37d3d82c99049f968180ae6132964a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-27 18:05:28 -0700

    structures

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,19 +1,5 @@
 #include "dog.h"
 #include <stdio.h>
-#ifndef DOG
-#define DOG
-/**
-* struct dog - define new type
-* @name: name
-* @age: age
-* @owner: owner
-*/
-/**struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};*/
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
