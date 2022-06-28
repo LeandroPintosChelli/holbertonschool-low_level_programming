edd1a1abd78c3d36e99aef9ad508589cb95aec67
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-27 18:17:35 -0700

    structures

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -6,20 +6,10 @@
 * @age: age
 * @owner: owner
 */
-struct dog
-{
-	char *name;
-	float age;
-	char *owner;
-};
-
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d == NULL)
-	return (NULL);
+	if (d != NULL)
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
-	return (d);
 }
-#endif
