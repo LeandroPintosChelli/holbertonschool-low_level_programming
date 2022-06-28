ae8e8b7538118787bb3cbef5a1a04612d83f8fdc
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-28 06:56:49 -0700

    structures

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -10,7 +10,7 @@ int _strlen(char *str)
 {
 	int count = 0;
 
-	for (count = 0; count < str; count++)
+	for (count = 0; str[count]; count++)
 		return (count);
 }
 /**
@@ -26,7 +26,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || owner == NULL)
 		return (NULL);
+
 	firulais = malloc(size_t(dog_t);
 	if (firulais ==  NULL)
-		return (NULL)
+		return (NULL);
 }
