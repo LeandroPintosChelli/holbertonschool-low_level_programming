6766aecbb4d94e125c01b93ec4d2f6e381d74140
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-28 18:31:28 -0700

    pointers_functions

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t count;
 
-	if (array == NULL && action == NULL)
+	if (array != NULL && action != NULL)
 		for (count = 0; count < size; count++)
 			action(*(array + count));
 }
