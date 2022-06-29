713d8820c924ac42f8c479f9f9a5dcdd29f60f1a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-29 09:35:35 -0700

    pointers_functions

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -24,9 +24,10 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	if ((*(get_op_func(argv[2]))) && (strlen(argv[2]) == 1))
-
+	{
 	res = (*(get_op_func(argv[2])))(atoi(argv[1]), atoi(argv[3]));
 		printf("%d\n", res);
+	}
 	else
 	{
 		printf("Error\n");
