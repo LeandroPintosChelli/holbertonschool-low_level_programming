8158e7178b877331344ba86d515070452dd7ef1b
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-29 08:48:36 -0700

    pointers_functions

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,6 @@
 #include "3-calc.h"
 #include <stdlib.h>
+#include <stdio.h>
 /**
 * get_op_func - check code
 * @s: char
---
+++ function_pointers/3-main.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "3-calc.h"
+#include <stdio.h>
+/**
+*
+*
+*/
+int main(int argc, char *argv[])
+{
+	int res;
+
+	if (argc != 4)
+		printf("Error\n");
+		exit(98);
+
+	if ((argv[2] = / || argv[2] = %) && (argv[3] = 0))
+		printf("Error\n");
+		exit(100);
+
+	if (get_op_func(argv[2]))
+
+	res = (*(get_op_func(argv[2]))(a, b))
+		printf("res\n");
+	else
+	{
+		exit(99);
+	}
+}
