de6ed23a7a42cd72670dd4055de42f862a85f3ac
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-29 06:05:31 -0700

    pointers_functions

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+* op_add - sum
+* @a: sum
+* @b: sum
+* Return: Always 0
+*/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+* op_sub - difference
+* @a: diff
+* @b: diff
+* Return: Always
+*/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+* op_mul - multiplicate
+* @a: mul
+* @b: mul
+* Return: Always 0
+*/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+* op_div - division
+* @a: div
+* @b: div
+* Return: Always 0
+*/
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+* op_mod - mod
+* @a: mod
+* @b: mod
+* Return: Always 0
+*/
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
