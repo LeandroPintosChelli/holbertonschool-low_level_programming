fb56943951b126f60b64f96de35683abf8dc16f5
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-28 18:22:36 -0700

    pointers_functions

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+* array_iterator - check code
+* @array: int
+* @size: size
+* @action: idk
+* Return: Always 0
+*/
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+	while (size > 0)
+	{
+		action(*array)
+		array++;
+	}
+}
