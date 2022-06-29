773fbeb4b82365b5ada567a9ec7ef809ee0b0089
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-29 09:31:17 -0700

    pointers_functions

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -21,10 +21,11 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (ops[i].op != NULL)
 	{
-		if (*(ops[i].op != *s))
-			continue;
+		if (*(ops[i].op) == s[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
 	}
-	else
-	{
-	return (ops[i].f);
+	return (NULL);
 }
