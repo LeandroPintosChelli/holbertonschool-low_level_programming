1b97d282e98f9b151d034f9d39f5c614ac0b43a3
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-29 09:23:17 -0700

    pointers_functions

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,27 +2,33 @@
 #include "3-calc.h"
 #include <stdio.h>
 /**
-*
-*
+* main - check code
+* @argc: int
+* @argv: char
+* Return: Always 0
 */
 int main(int argc, char *argv[])
 {
 	int res;
 
 	if (argc != 4)
+	{
 		printf("Error\n");
 		exit(98);
-
-	if ((argv[2] = / || argv[2] = %) && (argv[3] = 0))
+	}
+	if (((*argv[2] == '/') || (*argv[2] == '%')) && (atoi(argv[3]) == 0))
+	{
 		printf("Error\n");
 		exit(100);
+	}
+	if ((*(get_op_func(argv[2]))) && (strlen(argv[2]) == 1))
 
-	if (get_op_func(argv[2]))
-
-	res = (*(get_op_func(argv[2]))(a, b))
-		printf("res\n");
+	res = (*(get_op_func(argv[2])))(atoi(argv[1]), atoi(argv[3]));
+		printf("&d\n", res);
 	else
 	{
+		printf("Error\n");
 		exit(99);
 	}
+	return (0);
 }
