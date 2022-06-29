a65a3cf78c29d9d7fc0e00cafac71e5c6600996e
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-28 18:40:47 -0700

    pointers_functions

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,7 +17,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (count = 0; count < size; count++)
 
-	if (cmp(*array[count]))
+	if (cmp(array[count]))
 		return (count);
 
 	return (-1);
