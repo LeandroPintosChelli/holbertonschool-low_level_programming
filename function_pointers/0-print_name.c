4ec35a4a8bb841fc3768129364268fd31bb95351
LeandroPintosChelli <leandropintos162@gmail.com>
2022-06-28 18:12:56 -0700

    pointers_functions

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -2,11 +2,12 @@
 #include "function_pointers.h"
 #include <stdlib.h>
 /**
-*
-*
-*
+* print_name - check code
+* @name: char
+* @f: char
 */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
 	(*f)(name);
 }
