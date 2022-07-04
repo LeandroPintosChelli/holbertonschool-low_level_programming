e82a6ec5d77146be5344a75e794e5efee7fb847a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-04 12:43:34 -0700

    task101

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,15 +1,30 @@
 #include "main.h"
+void _numbers(unsigned int n);
 /**
 * print_number - check code
 * @n: print number
 */
 void print_number(int n)
 {
-	unsigned int num = n;
 	if (n < 0)
 	{
-		num = n * -1;
 		_putchar('-');
+		_numbers(-(unsigned int) n);
 	}
-
+	else
+	{
+		_numbers(n);
+	}
+}
+/**
+* _numbers - print numbers
+* @n: enteros
+*/
+void _numbers(unsigned int n)
+{
+	if (n / 10 != 0)
+	{
+		_numbers(n / 10);	
+	}
+	_putchar((n % 10) + '0');
 }
