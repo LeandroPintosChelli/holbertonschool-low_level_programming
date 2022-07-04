7d321c00b8ee6e7ffc0b56c94d6e7a958f8566b1
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-04 07:23:21 -0700

    variadic_functions

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -62,7 +62,7 @@ void print_all(const char * const format, ...)
 
 	va_start(all, format);
 
-	while (format[i] != '\0' && format != NULL)
+	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (lett[j].print != '\0')
