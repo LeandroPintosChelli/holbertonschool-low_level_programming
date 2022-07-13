1c8df04d5b78fd894515e040bec0aff29a76d057
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-12 17:58:05 -0700

    task

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,9 +4,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* *add_node - agrega nodo al inicio
+* add_node - agrega nodo al inicio
 * @head: head
 * @str: string
+* Return: *head
 */
 list_t *add_node(list_t **head, const char *str)
 {
