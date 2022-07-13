51334a2176659fa84f27a28cc5a599b20ef3034a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-12 17:33:42 -0700

    task

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*
+*
+*
+*
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *newlist;
+	char *strcp = strdup(str)
+
+	list_t *new list = malloc(sizeof(list_t));
+
+	if (newlist != NULL)
+	{
+		newlist->str = strcp;
+		newlist->next = head;
+		head = newlist;
+	}
+	return (newlist);
+}
--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -16,4 +16,5 @@ typedef struct node
 } list_t;
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
 #endif
