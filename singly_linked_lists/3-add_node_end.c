4b2b724b30556bc903a776b270ea287b4fbe0bf5
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-13 09:48:52 -0700

    task

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -12,20 +12,14 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newlist;
-	char *p = head;
+	list_t *he = i(*head);
 	newlist = malloc(sizeof(list_t));
 
-	while (*head->next != NULL)
-	{
-		p = p->next;
-		p->next = newnode;
-	}
 	if (newlist != NULL)
 	{
 		newlist->len = strlen(str);
 		newlist->str = strdup(str);
-		newlist->next = (*head);
-		(*head) = newlist;
+		newlist->next = NULL;
 	}
 	return (*head);
 }
