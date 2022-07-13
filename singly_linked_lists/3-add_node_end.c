cf5a04dae4bcb17f24729b0c0ae2b3ce32cd3ce7
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-13 07:52:56 -0700

    task

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -13,6 +13,7 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *newlist;
 	newlist = malloc(sizeof(list_t));
+	struct node *newNode = malloc(sizeof(struct node));
 
 	while (head->next != NULL)
 	{
