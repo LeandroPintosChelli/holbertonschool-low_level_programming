8d9f209890c571a86544e695315c3bc1f0c4837d
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 11:04:52 -0700

    more

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -28,7 +28,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (i < idx)
 	{
-		pos = pos->nest;
+		pos = pos->next;
 		i++;
 	}
 	while( j < idx)
@@ -38,6 +38,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	newnode->n = n;
 	newnode->next = pos;
-	bef->nex = newnode;
+	b->nex = newnode;
 	return (newnode);
 }
