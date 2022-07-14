397a62c1332de42c699459b31fc9a97d662f637e
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 07:54:03 -0700

    more

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -18,7 +18,6 @@ listint_t *add_nodeint(listint_t **head, const int n)
 
 	if (newlist != NULL)
 	{
-		newlist->n = strcp;
 		newlist->len = strlen(n);
 		newlist->next = (*head);
 		(*head) = newlist;
