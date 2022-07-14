04d9c015ab9f8cf7443f3101b890a6358ead6db6
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 07:45:21 -0700

    more

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+* list_len - count number of elements
+* @h: list pointer
+* Return: count
+*/
+size_t listint_len(const listint_t *h)
+{
+	int count = 0;
+
+	if (h == NULL)
+		return (count);
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
