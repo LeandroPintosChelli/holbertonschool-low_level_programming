2bada82dfa0b7331cd5f0229ca02c142b9888b35
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 08:15:00 -0700

    more

--- more_singly_linked_lists/3-add_nodeint_end.c
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *newlist;
 	listint_t *he = (*head);
 
-	newlist = malloc(sizeof(list_t));
+	newlist = malloc(sizeof(listint_t));
 	if (!newlist)
 	{
 		return (0);
