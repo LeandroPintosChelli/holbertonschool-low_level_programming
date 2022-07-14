8f0c669c54b5bd8f3e1a0f86ad531e788f968cbe
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 09:47:22 -0700

    more

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -12,7 +12,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int count = 0;
-	listint_t *p = (*head);
+	listint_t *p;
 
 	while (p != NULL)
 	if (count == index)
