8cf5101548ac2ad5a3eb77fc458cdc3910ef3525
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 07:50:58 -0700

    more

--- more_singly_linked_lists/2-add_nodeint.c
+++ more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	list_t *newlist;
 	char *strcp = strdup(n);
 
-	newlist = malloc(sizeof(list_t));
+	newlist = malloc(sizeof(listint_t));
 
 	if (newlist != NULL)
 	{
