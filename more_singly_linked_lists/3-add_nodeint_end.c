8e4eb05346e73ceb8f67cfbfd3ec089217bf3dbf
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 08:13:48 -0700

    more

---
+++ more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* add_nodeint_end - agrega nodo al final
+* @head: head
+* @n: string
+* Return: *head
+*/
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	list_t *newlist;
+	list_t *he = (*head);
+
+	newlist = malloc(sizeof(list_t));
+	if (!newlist)
+	{
+		return (0);
+	}
+
+		newlist->n = strdup(n);
+		newlist->len = strlen(n);
+		newlist->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = newlist;
+		return (*head);
+	}
+	while (he != NULL && he->next !=NULL)
+	{
+		he = he->next;
+	}
+	he->next = newlist;
+	return (*head);
+}
