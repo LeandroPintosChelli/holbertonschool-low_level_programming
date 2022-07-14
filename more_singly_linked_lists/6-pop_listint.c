15215f8a71ece8fd8dfe2dc1b6db70d2e675a413
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 09:15:38 -0700

    more

---
+++ more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+/**
+* pop_listint_end - agrega nodo al final
+* @head: head
+* Return: *head
+*/
+int pop_listint(listint_t **head)
+{
+	n = head;
+	*head = ((*head)->next
+	free(n);
+}
