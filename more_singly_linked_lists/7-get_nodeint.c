3fa6d7c4fce2236ec910e04cfc232c3a45ed51e2
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-14 09:46:50 -0700

    more

---
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+/**
+* get_nodeint_at_index - agrega nodo al index
+* @head: head
+* @index: int
+* Return: *head
+*/
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+	listint_t *p = (*head);
+
+	while (p != NULL)
+	if (count == index)
+	{
+		return (p->next);
+	}
+	count++;
+	p = p->next;
+}
