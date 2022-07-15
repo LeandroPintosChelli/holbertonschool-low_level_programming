ce35b8738c44a353e5795a8ca4c5a0887833aa6a
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-15 07:53:58 -0700

    more

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -12,41 +12,35 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int i = 0;
+	unsigned int i;
 	listint_t *pos, *bef;
 
 	bef = *head;
 
 	pos = malloc(sizeof(listint_t));
-	while (i < idx)
+
+	while (head == NULL)
 	{
-		if (pos == NULL)
-		{
-			return (NULL);
-		}
-		pos->n = n;
-		pos->next = 0;
-	}
+		return (NULL);
+
+	pos->n = n;
+	pos->next = NULL;
+
 	if (idx == 0)
 	{
 		pos->next = *head;
 		*head = pos;
 		return (pos);
 	}
-	for (i = 0; bef == NULL; i++)
-	{
-	if (!bef)
-	{
-		return (NULL);
-	}
-	pos->n = n;
-	pos->next = pos;
 
-	if (bef)
+	for (i = 0; bef != NULL && i <= idx; i++)
 	{
-		bef->next = pos;
-		*head = pos;
-	}
+		if (i <= idx)
+		{
+			pos->next = bef->next;
+			bef->next = pos;
+			return (pos);
+		}
 	}
 	return (NULL);
 }
