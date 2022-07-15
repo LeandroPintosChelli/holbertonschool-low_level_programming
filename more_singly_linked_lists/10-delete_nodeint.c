2f32d8eb5c300aad5f663621ed9db9a2c4ed35e9
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-15 10:12:05 -0700

    more

--- more_singly_linked_lists/10-delete_nodeint.c
+++ more_singly_linked_lists/10-delete_nodeint.c
@@ -14,18 +14,26 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	unsigned int idx = 0;
 
-	while (tmp && idx < index -1)
-	{
-		tmp = tmp->next;
-	}
-	if (!tmp || !tmp->next)
+	if (!tmp)
+		return (-1);
+	if (index == 0)
 	{
 		*head = tmp->next;
 		free(tmp);
+		return (1);
+	}
+	while (tmp && idx < index -1)
+	{
+		tmp = tmp->next;
+		idx++;
+	}
+	if (!tmp || !(tmp->next))
+	{
 		return (-1);
 	}
 	prev = tmp->next;
 	tmp->next = prev->next;
+
 	free(prev);
 	return (+1);
 }
