d7f36e66f9713c3a5a2c6128b8ad2934afbcfbbe
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-15 07:22:16 -0700

    more

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -27,7 +27,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		pos->n = n;
 		pos->next = 0;
 	}
-	if (idx == 0)
+	if (idx == NULL)
 	{
 		pos->next = *head;
 		*head = pos;
