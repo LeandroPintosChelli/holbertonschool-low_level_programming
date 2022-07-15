3c364d1570725c723b81a2912a10c5ed7bf51ab4
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-15 08:01:51 -0700

    more

--- more_singly_linked_lists/9-insert_nodeint.c
+++ more_singly_linked_lists/9-insert_nodeint.c
@@ -19,7 +19,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	pos = malloc(sizeof(listint_t));
 
-	while (head == NULL)
+	while (head == NULL && pos == NULL)
 	{
 		return (NULL);
 	}
