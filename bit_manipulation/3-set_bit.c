2e8f961654efdb4fe509250eeebf421cfb451e08
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-18 12:23:02 -0700

    task 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* clear_bit - returns the value of a bit
+* @n: unsigned long int
+* @index: unsigned int
+* Return: n & 1
+*/
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int cont;
+
+	if (index > 64)
+		return (-1);
+
+	for (cont = 1; index > 0; index--, cont *= 2)
+		;
+	*n += cont;
+	return (1);
+}
