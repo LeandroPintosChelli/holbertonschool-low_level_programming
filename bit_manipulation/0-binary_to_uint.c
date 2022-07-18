5bc436ab1df48e95450b045394a2c17bc1182eb0
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-18 10:40:25 -0700

    task 0

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -11,11 +11,19 @@ unsigned int binary_to_uint(const char *b)
 
 	if (b == NULL)
 		return (0);
-
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		i++;
+	}
+	i = 0;
 	while (b[i] == '0' || b[i] == '1')
 	{
 		val <<= 1;
-		val += b[i]- '0';
+		val += b[i] - '0';
 		i++;
 	}
 	return (val);
