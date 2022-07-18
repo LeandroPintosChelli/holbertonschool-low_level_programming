9e4588d9ebb5e547faeda385ed70d728956b8b90
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-18 13:55:57 -0700

    task 5

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -7,8 +7,8 @@
 */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int cont = 0;
-	int i;
+	int cont = 0;
+	unsigned long int i;
 
 	for (i = n ^ m; i != 0; i = i & (i - 1))
 		cont++;
