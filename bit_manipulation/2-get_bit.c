64ea654e5264c7971d41c2d34eb34d0588e1c394
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-18 12:05:08 -0700

    task 2

---
+++ bit_manipulation/2-get_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* get_bit - returns the value of a bit
+* @n: unsigned long int
+* @index: unsigned int
+* Return: n & 1
+*/
+int get_bit(unsigned long int n, unsigned int index)
+{
+	unsigned long int cont;
+
+	if (index > 64)
+		return (-1);
+
+	cont = n >> index;
+	return (cont & 1);
+}	
