28d6f33db65704d0a814b0d3a82e1a13f75c0cfa
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-19 10:43:37 -0700

    task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
-*
-*
-*
-*
+* create_file - create file
+* @filename: file name
+* @text_content: content
+* Return: 1 or -1
 */
 int create_file(const char *filename, char *text_content)
 {
