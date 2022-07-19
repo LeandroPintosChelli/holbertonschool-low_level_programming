8567206d9f60a22cccba9bd21b0e4b8e960ceb71
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-19 10:41:55 -0700

    task

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+*
+*
+*
+*
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; len++)
+		;
+	}
+
+	o = open (filename, O_CREAT | O_RDWR, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+	return (1);
+}
