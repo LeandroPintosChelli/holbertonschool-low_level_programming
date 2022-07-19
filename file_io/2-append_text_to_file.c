ef107c1eaa01909ea7750ac04a22e8b3dc3cfc35
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-19 11:19:00 -0700

    task

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
-* create_file - create file
-* @filename: file name
+* append_text_to_file - append text
+* @filename: const char
 * @text_content: content
 * Return: 1 or -1
 */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	int o, w, len = 0;
 
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 		;
 	}
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
