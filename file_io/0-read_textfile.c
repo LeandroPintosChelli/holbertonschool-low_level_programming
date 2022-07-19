4f8a97732d17e898d258334ff4d263845f9d8ae2
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-19 10:03:03 -0700

    task

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int o, r, w;
+	char *buff;
+
+	if (filename == NULL)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buff, letters);
+	w = write(STDOUT_FILENO, buff, r);
+
+	if (o == -1 || r == -1 || w == -1)
+
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(o);
+
+	return (w);
+}
