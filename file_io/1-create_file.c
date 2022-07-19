b9a6436fca2b12084a1055eddd0d5bc46adeb6e4
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-19 10:46:43 -0700

    task

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		for (len; text_content[len]; len++)
+		for (len = 0; text_content[len]; len++)
 		;
 	}
 
