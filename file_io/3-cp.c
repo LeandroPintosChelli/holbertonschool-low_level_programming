a9fcb24b6e427677de8d756892a40c03bfc67645
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-20 13:37:57 -0700

    task

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,7 +6,7 @@
 * @fd: file descriptor
 * Return: 0 if is success
 */
-int _exit(int error, char *s, int fd)
+int __exit(int error, char *s, int fd)
 {
 	switch (error)
 	{
@@ -38,27 +38,27 @@ int main(int argc, char *argv[])
 	char *buffer[1024];
 
 	if (argc != 3)
-		_exit(97);
+		__exit(97);
 
 	o = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (o == -1)
-	_exit(99, argv[2], 0);
+	__exit(99, argv[2], 0);
 
 	j = open(argv[1], O_RDONLY);
 	if (j == -1)
-		_exit(98, argv[1], 0);
+		__exit(98, argv[1], 0);
 
 	while ((r = read(j, buffer, 1024)) != 0)
 	{
 		if (r == -1)
-			_exit(98, argv[1], 0);
+			__exit(98, argv[1], 0);
 
 	w = write(o, buffer, r);
 	if (w == -1)
-		_exit(99, argv[2], 0);
+		__exit(99, argv[2], 0);
 	}
 
-	close(o) == -1 ? (_exit(100, NULL, o)) : close(o);
-	close(j) == -1 ? (_exit(100, NULL, j)) : close(j)
+	close(o) == -1 ? (__exit(100, NULL, o)) : close(o);
+	close(j) == -1 ? (__exit(100, NULL, j)) : close(j)
 	return (0);
 }
