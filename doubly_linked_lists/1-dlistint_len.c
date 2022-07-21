263df6de1839aac8950d5865b410f41663c065d2
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-21 06:48:31 -0700

    task 1

---
+++ doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+* print_dlistint - print size of a list
+* @h: number
+* Return: len
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int len = 0;
+
+	while (h != NULL)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
