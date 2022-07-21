1ffc5cc51a830df2c52632794a41334cd110a455
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-21 06:40:11 -0700

    task 0

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -8,20 +8,12 @@
 size_t print_dlistint(const dlistint_t *h)
 {
 	int count = 0;
-	const dlistint_t *ct = h;
 
-	for (; ct; ct = ct->next)
+	while (h)
 	{
-	if (ct->prev)
-	{
-	printf("%u", ct->n);
-	}
-	else
-	{
-	printf("9");
-	}
-	count++;
-	putchar('\n');
+		count++;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 	return (count);
 }
