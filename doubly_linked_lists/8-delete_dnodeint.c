b4284ec6ec60e68da451608ddb347815996ae5b0
LeandroPintosChelli <leandropintos162@gmail.com>
2022-07-22 08:01:36 -0700

    task 8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,6 +9,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
+	dlistint_t *aft;
 
 	unsigned int idx = 0;
 
@@ -18,27 +19,24 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	{
 		*head = tmp->next;
 		free(tmp);
+		(*head)->prev = NULL;
 		return (1);
 	}
 	while (tmp && idx < index - 1)
 	{
-		if (tmp == NULL)
-			return (-1);
 		tmp = tmp->next;
 		idx++;
 	}
-		if (tmp == *head)
-		{
-			*head = tmp->next;
-			if (*head != NULL)
-				(*head)->prev = NULL;
-		}
-		else
-		{
-		tmp->prev->next = tmp->next;
-		if (tmp->next != NULL)
-			tmp->next->prev = tmp->prev;
-		}
-	free(tmp);
+	if (!tmp || !(tmp->next))
+	{
+		return (-1);
+	}
+	aft = tmp->next;
+	tmp = aft->prev;
+	tmp->next = aft->next;
+	aft->next->prev = tmp->prev;
+
+	free(prev);
+	aft = NULL;
 	return (+1);
 }
