0a3ce8bde65a803d1858f880e73fc374f0a49b38
LeandroPintosChelli <leandropintos162@gmail.com>
2022-08-19 12:19:55 -0700

    Add 3-hash_table_set.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,7 +11,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newnode;
 	char *value_copy;
-	unsigned long int index, i;
+	unsigned long int index, size;
 
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
