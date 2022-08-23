401fc2f04cd212acefd3301ba8249a80e2b566c1
LeandroPintosChelli <leandropintos162@gmail.com>
2022-08-23 13:38:25 -0700

    Add all the files of exercises

---
+++ search_algorithms/search_algos.h
@@ -0,0 +1,8 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+#endif
